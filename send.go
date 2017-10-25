586373384a5bda0c5d4cfb6cc40de0ee5c810e0d
ThomasHuke <thomashuke@icloud.com>
2017-10-25 14:52:19 +0800

    change

--- send.go
+++ send.go
@@ -5,13 +5,23 @@ import (
 	"strings"
 )
 
-func sendToMail(user, password, host, to, subject, body, mailtype string) error {
+func htmlH(mailType string) bool {
+	if mailType == "html" {
+		return true
+	} else if mailType == "Html" {
+		return true
+	} else {
+		return false
+	}
+}
+
+func sendToMail(user, password, host, to, subject, body, mailType string) error {
 	hp := strings.Split(host, ":")                    // 分割字符串
 	auth := smtp.PlainAuth("", user, password, hp[0]) //返回一个auth的interface的实例。这个interface内容是nil
 	//实际上就是一个实现了next和start的一个struct。这里取hp[0]是因为传入的只是一个hostname并不需要端口，故。
 	var contentType string
-	if mailtype == "html" { //mailtype是邮件的类型。
-		contentType = "Content-Type: text/" + mailtype + "; charset=UTF-8" //如果是HTML那么头文件就是
+	if htmlH(mailType) { //mailtype是邮件的类型。
+		contentType = "Content-Type: text/" + mailType + "; charset=UTF-8" //如果是HTML那么头文件就是
 		// Content-Type: text/html; charset=UTF-8
 	} else {
 		contentType = "Content-Type: text/plain" + "; charset=UTF-8" //
--- sendMail.go
+++ sendMail.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 )
 
+/*
+Run 函数暴露在外，是为了接入 user to password subject body mailType等信息。
+通常来说user就是你的用户名，password是该邮件的密码。host是smtp提供的一个address subject是邮件的主题
+to是收件人，body是内容，maiType是发送邮件的类型通常是Html和Plain
+*/
 func Run(user, to, password, host, subject, body, mailType string) {
 	fmt.Println("send email")
 	err := sendToMail(user, password, host, to, subject, body, mailType)
